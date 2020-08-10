970b7b6d04ccadf58ab21997484a314b75b4dcac
Atheane <eroyant@gmail.com>
2020-08-11 00:46:38 +0200

    translate

--- src/app/format.js
+++ src/app/format.js
@@ -1,10 +1,10 @@
 export const formatDate = (dateStr) => {
   const date = new Date(dateStr)
-  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
-  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
-  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
+  const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(date)
+  const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
   const month = mo.charAt(0).toUpperCase() + mo.slice(1)
-  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+  return `${month.substr(0,3)}. ${parseInt(da)}, ${ye.toString().substr(0,4)}`
 }
  
 export const formatStatus = (status) => {
